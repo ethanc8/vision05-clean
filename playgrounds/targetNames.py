3dc52f1fa91bedd326199e1f4fc07fd817d65bb1
titan2022-multiuser-system <titan2022.multiuser.system@gmail.com>
2024-02-10 11:45:57 -0600

    Add Unknown to target names

--- playgrounds/targetNames.py
+++ playgrounds/targetNames.py
@@ -1,9 +1,11 @@
 nameForTarget = {
     "ethanc8": "Ethan Charoenpitaks",
-    "vikram": "Vikram Rao"
+    "vikram": "Vikram Rao",
+    "Unknown": "",
 }
 
 numberForTarget = {
     "ethanc8": 99,
-    "vikram": 1
+    "vikram": 1,
+    "Unknown": "Unknown",
 }
