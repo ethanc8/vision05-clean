fd97c6df8c6ad93c9e752251a596cd155171e4bc
ethanc8 <echaroenpitaks@gmail.com>
2024-02-12 17:38:55 -0600

    Choose the person that we have highest confidence in and require at least 65% confidence

--- playgrounds/detectFaces.py
+++ playgrounds/detectFaces.py
@@ -64,14 +64,18 @@ while True:
 	# compute the facial embeddings for each face bounding box
 	encodings = face_recognition.face_encodings(rgb, boxes)
 	names = []
+	confidences = []
 
 	# loop over the facial embeddings
 	for encoding in encodings:
 		# attempt to match each face in the input image to our known
 		# encodings
 		matches = face_recognition.compare_faces(data["encodings"],
+			encoding, tolerance=0.35)
+		distances = face_recognition.face_distance(data["encodings"],
 			encoding)
 		name = "Unknown"
+		confidence = 0
 
 		# check to see if we have found a match
 		if True in matches:
@@ -80,17 +84,23 @@ while True:
 			# was matched
 			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
 			counts = {}
+			bestDistance = {}
 
 			# loop over the matched indexes and maintain a count for
 			# each recognized face face
 			for i in matchedIdxs:
 				name = data["names"][i]
 				counts[name] = counts.get(name, 0) + 1
+				if name in bestDistance.keys():
+					bestDistance[name] = min(distances[i], bestDistance[name])
+				else:
+					bestDistance[name] = distances[i]
 
 			# determine the recognized face with the largest number
 			# of votes (note: in the event of an unlikely tie Python
 			# will select first entry in the dictionary)
-			name = max(counts, key=counts.get)
+			name = min(bestDistance, key=bestDistance.get)
+			confidence = 1 - bestDistance[name]
 			
 			#If someone in your dataset is identified, print their name on the screen
 			if currentname != name:
@@ -107,15 +117,21 @@ while True:
 				
 		# update the list of names
 		names.append(name)
+		confidences.append(confidence)
 
 	# loop over the recognized faces
-	for ((top, right, bottom, left), name) in zip(boxes, names):
+	for ((top, right, bottom, left), name, confidence) in zip(boxes, names, confidences):
 		# draw the predicted face name on the image - color is in BGR
 		cv2.rectangle(frame, (left, top), (right, bottom),
 			(0, 255, 225), 2)
 		y = top - 15 if top - 15 > 15 else top + 15
-		cv2.putText(frame, f"({numberForTarget[name]}) {nameForTarget[name]}", (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-			.8, (0, 255, 255), 2)
+		# f"({numberForTarget[name]}) {nameForTarget[name]}",
+		if confidence > 0.65:
+			text = f"{confidence * 100 :.2f}% {nameForTarget[name]}"
+		else:
+			text = "Unknown"
+		cv2.putText(frame, text, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+			0.8, (0, 255, 255), 2)
 
 	# display the image to our screen
 	cv2.imshow("Facial Recognition is Running", frame)
